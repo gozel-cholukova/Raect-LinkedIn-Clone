b626511df2d18386d1b585a0674912a845d0d9aa
Gozel Cholukova <gozelr@mail.ru>
2021-01-31 01:07:15 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from "./Feed";
-import { selectUser } from './features/userSlice';
-import { useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
 import Login from "./Login";
+import { auth } from './firebase';
 // import Widgets from "./Widgets";
 
 function App() {
-  const user = useSelector(selectUser)
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        // user is logged in
+        dispatch(login({
+          email: userAuth.email,
+          uid: userAuth.uid,
+          displayName: userAuth.displayName,
+          photoUrl: userAuth.photoURL,
+        }))
+      } else {
+        // user is logged out
+        dispatch(logout());
+      }
+    });
+  }, []);
+
   return (
     <div className="app">
       {/* Header */}
