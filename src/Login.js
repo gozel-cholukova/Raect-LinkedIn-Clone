909d6f43153a22cecadcb1e2f7f8be5653e9bcda
Gozel Cholukova <gozelr@mail.ru>
2021-02-01 01:17:04 +0500

    Add files via upload

--- src/Login.js
+++ src/Login.js
@@ -39,6 +39,17 @@ function Login() {
 
   const loginToApp = (e) => {
     e.preventDefault();
+
+    auth.signInWithEmailAndPassword(email, password)
+    .then(userAuth => {
+      dispatch(login({
+        email: userAuth.user.email,
+        uid: userAuth.user.uid,
+        display: userAuth.user.displayName,
+        photoUrl: userAuth.user.photoURL,
+      }))
+    })
+    .catch((error) => alert(error));
   };
 
   return (
